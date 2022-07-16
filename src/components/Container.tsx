7a2949da2ddf44d31a16ed503bb6aaf5b9aaa581
akabeko <akabekobeko@gmail.com>
2022-07-16 18:16:14 +0900

    feat: basic component design

---
+++ src/components/Container.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import { Presenter } from './Presenter'
+
+/**
+ * Components that care about what data is shown to the user.
+ */
+export const Container: FC = () => {
+  return <Presenter />
+}
---
+++ src/components/Presenter.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+
+/**
+ * Components that care about how data is shown to the user.
+ */
+export const Presenter: FC = () => {
+  return <div>Sample Page</div>
+}
