6e4d7e7716b693b8a01b564050aa8901dc753636
akabeko <akabekobeko@gmail.com>
2022-07-17 00:52:58 +0900

    feat: added an input for the account and password

--- src/components/UserRegistration/Presenter.tsx
+++ src/components/UserRegistration/Presenter.tsx
@@ -1,8 +1,56 @@
 import { FC } from 'react'
+import { Box, Typography, TextField } from '@mui/material'
+import { styled } from '@mui/system'
+
+const Panel = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '.5rem',
+})
 
 /**
  * Components that care about how data is shown to the user.
  */
 export const Presenter: FC = () => {
-  return <div>Sample Form</div>
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        padding: '1.5rem',
+        backgroundColor: '#FBFCFA',
+        color: '#34495E',
+      }}
+    >
+      <Typography
+        variant="h1"
+        sx={{
+          textAlign: 'center',
+        }}
+      >
+        User Registration
+      </Typography>
+      <Box
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: '1.5rem',
+          gap: '1rem',
+          border: 'solid 2px #34495E',
+          borderRadius: '.3rem',
+        }}
+      >
+        <Panel>
+          <Typography variant="h3">Account</Typography>
+          <TextField placeholder="Account Name" />
+        </Panel>
+        <Panel>
+          <Typography variant="h3">Password</Typography>
+          <TextField placeholder="Password" type="password" />
+        </Panel>
+      </Box>
+    </Box>
+  )
 }
