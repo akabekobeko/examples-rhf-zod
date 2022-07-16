676a2f249bed8dd62ddbc346169ea78b84ed3410
akabeko <akabekobeko@gmail.com>
2022-07-16 18:48:55 +0900

    chore: organize component references

--- src/components/SampleForm/Presenter.tsx
+++ src/components/SampleForm/Presenter.tsx
@@ -4,5 +4,5 @@ import { FC } from 'react'
  * Components that care about how data is shown to the user.
  */
 export const Presenter: FC = () => {
-  return <div>Sample Page</div>
+  return <div>Sample Form</div>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from 'next'
-import { Container } from '@components/Container'
+import { Box } from '@mui/material'
+import { SampleFormContainer } from '@components'
 
-const Home: NextPage = () => <Container />
+/**
+ * Component fot het home page.
+ */
+const Home: NextPage = () => (
+  <Box sx={{ width: '100wh', height: '100vh' }}>
+    <SampleFormContainer />
+  </Box>
+)
 
 export default Home
