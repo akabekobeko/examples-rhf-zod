48c726ce59aaee1a5fde21c245edb7f8d1dd1a86
akabeko <akabekobeko@gmail.com>
2022-07-17 00:51:57 +0900

    feat: added the theme setting

---
+++ src/pages/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material'
+
+/**
+ * Unified look and feel of the application.
+ */
+export const theme = createTheme({
+  typography: {
+    h1: {
+      fontSize: '2rem',
+    },
+    h2: {
+      fontSize: '1.5rem',
+    },
+    h3: {
+      fontSize: '1.3rem',
+    },
+    h4: {
+      fontSize: '1.2rem',
+    },
+  },
+})
